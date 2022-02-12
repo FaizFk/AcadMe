36ed4ee42829d643f0827ad76780c0839bf1b8c6
Lalit <lalitkumarsingh3716@gmail.com>
2022-02-12 17:16:40 +0530

    feat(ui): Added Navbar

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,33 @@
-module.exports.renderIndex = (_req, res) => {
+module.exports.renderIndex = async (req, res) => {
+  const search = req.query.searchTerm || "";
+  const sortBy = req.query.filter?.sort || "upvotes";
+  let tags = req.query.filter?.tags ||
+    req.user?.foi || [
+      "Computer Science",
+      "Mathematics",
+      "Literature",
+      "Mechanics",
+    ];
+
+  if (typeof tags === "string") {
+    tags = [tags];
+  }
+
+  let type = req.query.filter?.type || ["video", "pdf"];
+
+  if (typeof type === "string") {
+    type = [type];
+  }
+
+  // Fetch Resources
+
   res.render("pages/home", {
     page: "home",
+    searchTerm: req.query.searchTerm,
+    filter: {
+      sortBy,
+      tags,
+      type,
+    },
   });
 };
