fd2943598fc0066c0cacb600d2da9135ef642107
Lalit <lalitkumarsingh3716@gmail.com>
2022-02-12 16:00:09 +0530

    feat(backend): Added Authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,43 @@
+const User = require("../models/users");
+
 module.exports.renderRegisterPage = (_req, res) => {
   res.render("auth/register", { page: "register" });
 };
 
 module.exports.renderLoginPage = (_req, res) => {
   res.render("auth/login", { page: "login" });
+};
+
+module.exports.registerUser = async (req, res, next) => {
+  try {
+    const { email, username, password, foi, cpassword } = req.body;
+
+    if (password !== cpassword) {
+      req.flash("error", "Passwords do not match");
+      return res.redirect("/auth/register");
+    }
+
+    const user = new User({ email, username, foi });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to KSP-APP");
+      res.redirect("/");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/auth/register");
+  }
+};
+
+module.exports.loginUser = (req, res) => {
+  const redirectUrl = req.session.returnTo || "/";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
+};
+
+module.exports.logoutUser = (req, res) => {
+  req.logout();
+  req.flash("success", "Successfully Logged Out");
+  res.redirect("/auth/login");
 };
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const ejsMate = require("ejs-mate");
+const { db } = require("./configs/dbConfig");
+const session = require("./configs/sessionConfig");
+const passport = require("./configs/passportConfig");
+const flash = require("./configs/flashConfig");
 
 const app = express();
+
+// DB Connection
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+  console.log("DATABASE CONNECTED!!");
+});
 
 // Body-parsers and Static files
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +26,11 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
+
+// Configs
+session(app);
+passport(app);
+flash(app);
 
 // Routes
 app.use("/", require("./routes/index"));
